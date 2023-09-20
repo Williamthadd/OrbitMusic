4bb7d9b3b152479f7a1430fa3a868353342404fd
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-09-20 19:29:23 +0700

    MODIFIED - Service Details

--- src/App.js
+++ src/App.js
@@ -11,12 +11,6 @@ import Login from "./Pages/Login";
 
 import ServiceDetails from "./Pages/ServiceDetails";
 
-import AttributeDesignDetails from "./Pages/ServiceDetails/AtributeDesignDetail";
-import ConsultingDetail from "./Pages/ServiceDetails/ConsultingDetail";
-import ProducingDetail from "./Pages/ServiceDetails/ProducingDetail";
-import PromotionDetail from "./Pages/ServiceDetails/PromotionDetail";
-import TalentManagementDetail from "./Pages/ServiceDetails/TalentManagementDetail";
-
 import "./Styles/style.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
@@ -44,23 +38,9 @@ function App() {
           <Route path="/success" element={<PaymentSuccessful />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          {/*  */}
-          {/* Service Details Route */}
-          {/*  */}
           <Route
             path="/services/details"
             element={<ServiceDetails currentPage={currentPage} />}
-          />
-          <Route
-            path="/services/AttributeDesign"
-            element={<AttributeDesignDetails />}
-          />
-          <Route path="/services/Consulting" element={<ConsultingDetail />} />{" "}
-          <Route path="/services/Producing" element={<ProducingDetail />} />{" "}
-          <Route path="/services/Promotion" element={<PromotionDetail />} />{" "}
-          <Route
-            path="/services/TalentManagement"
-            element={<TalentManagementDetail />}
           />
         </Routes>
       </Router>
