a0fd74f4f2246ff95d6cdfdc65efee447f4f8ed0
William Thaddeus <88379454+Williamthadd@users.noreply.github.com>
2023-09-21 10:09:54 +0700

    Update Profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,13 +1,94 @@
+import React, { useState } from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
 export default function Profile() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  //dummy
+  const username = "Christian Nabil";
+  const email = "nabil@gmail.com";
+  const phoneNumber = "+62 9612737123";
+  //dummy
+
   return (
     <div className="container">
       <Header />
       <div className="flex jc-c page-title">
         <h1>Profile</h1>
       </div>
+
+      <div className="Profile-Data">
+        <p><b>Username</b>: {username}</p> 
+        <p><b>Email</b>: {email}</p>
+        <p><b>Phone Number</b>: {phoneNumber}</p>
+      </div>
+
+      <div className="Update-Profile-Box">
+        <h2 className="Update-Profile-Title">Update Profile?</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group-Update-Profile">
+            <label htmlFor="username">Username:</label>
+            <input
+              className="Input-Profile"
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group-Update-Profile">
+            <label htmlFor="email">Email:</label>
+            <input
+              className="Input-Profile"
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group-Update-Profile">
+            <label htmlFor="password">Password:</label>
+            <input
+              className="Input-Profile"
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group-Update-Profile">
+            <label htmlFor="phoneNumber">Phone Number:</label>
+            <input
+              className="Input-Profile"
+              type="tel"
+              id="phoneNumber"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit" className="button1 form-group-Update-Profile">Update</button>
+        </form>
+      </div>
+
       <Footer />
     </div>
   );
